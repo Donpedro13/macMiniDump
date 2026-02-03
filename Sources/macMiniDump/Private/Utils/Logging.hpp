b8c2bdbb11ceda3cd3c6eb79f967bf99e0a9549a
Donpedro13 <2521782+Donpedro13@users.noreply.github.com>
2026-02-03 21:14:12 +0100

    Rework logging infrastructure - The current logging infrastructure was simplified, and is no longer exposed to clients   - Rationale: in its current structure, it would not have been possible to use this piece of functionality without heap allocations (e.g. string concatenations) in release builds, which is something we want to avoid - The new, macro-based logging is much simpler, and is completely omitted in non-debug builds - Rewriting ad-hoc logging throughout the library will soon follow

---
+++ Sources/macMiniDump/Private/Utils/Logging.hpp
@@ -0,0 +1,40 @@
+#ifndef MMD_LOGGING
+#define MMD_LOGGING
+
+#pragma once
+
+#include <iostream>
+
+namespace MMD {
+
+#ifndef NDEBUG
+class DebugLogLine {
+public:
+    DebugLogLine() { std::cerr << "[DEBUG] "; }
+    ~DebugLogLine() { std::cerr << std::endl; }
+    
+    template<typename T>
+    DebugLogLine& operator<<(const T& value) {
+        std::cerr << value;
+        return *this;
+    }
+    
+    DebugLogLine& operator<<(std::ostream& (*manip)(std::ostream&)) {
+        manip(std::cerr);
+        return *this;
+    }
+};
+#endif // NDEBUG
+
+} // namespace MMD
+
+#ifdef NDEBUG
+    // Expressions are not evaluated in release builds
+    #define MMD_DEBUGLOG if (true) {} else std::cerr
+    #define MMD_DEBUGLOG_LINE if (true) {} else std::cerr
+#else
+    #define MMD_DEBUGLOG std::cerr << "[DEBUG] "
+    #define MMD_DEBUGLOG_LINE MMD::DebugLogLine()
+#endif
+
+#endif // MMD_LOGGING
